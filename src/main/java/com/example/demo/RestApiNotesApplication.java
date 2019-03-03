0c2f734e7d973141c692407dfa3c564a9c66f0ad
thatvictor7 <montoya.victor7@gmail.com>
2019-03-02 22:05:45 -0700

    intial commit

---
+++ src/main/java/com/example/demo/RestApiNotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiNotesApplication.class, args);
+	}
+
+}
